03aa66d451c71df1628f53940f6bca3363cce5dd
versionFinalAhorcado <juan.jimenez.toro@correounivalle.edu.co>
2025-09-09 17:18:01 -0500

    Creación de repositorio

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>DS2 Task Manager</h1>
+    <p>Application is loading...</p>
+  </div>
+`;
+
+console.log('DS2 Task Manager initialized');
